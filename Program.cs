9a5a974581a89bd8df36e02e334f5de61e7c3a94
veles38x <kirillst2005@gmail.com>
2022-08-12 00:24:36 +0300

    ГОТОВО... вроде

--- Program.cs
+++ Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿Console.WriteLine("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int a = num/100;
+int b = num/10-a*10;
+int c = num-a*100-b*10;
+
+Console.WriteLine(b);
